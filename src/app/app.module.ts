e0c43ff62a87f560ded96089432b719e7d0e41cb
cpmoviephil <you9215712@gmail.com>
2020-12-02 20:57:07 +0800

    stations components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShowStationsComponent } from './show-stations/show-stations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowStationsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/show-stations/show-stations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-stations',
+  templateUrl: './show-stations.component.html',
+  styleUrls: ['./show-stations.component.css']
+})
+export class ShowStationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
